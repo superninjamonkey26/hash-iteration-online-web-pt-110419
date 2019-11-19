c76613aebc7d3ccb2b4369701beeb22f26fe1f8f
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-11-19 04:25:16 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,7 +12,7 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    if (age<10)
+    if (age < 10)
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
